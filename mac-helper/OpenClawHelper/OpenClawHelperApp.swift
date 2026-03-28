c38252d8ec78599ca80e8af55522b256785fbfe0
bumpkingsol <sorensen448@gmail.com>
2026-03-28 16:50:25 +0100

    feat: rename helper app to ClawRelay

--- mac-helper/OpenClawHelper/OpenClawHelperApp.swift
+++ mac-helper/OpenClawHelper/OpenClawHelperApp.swift
@@ -5,12 +5,12 @@ struct OpenClawHelperApp: App {
     @StateObject private var appModel = AppModel()
 
     var body: some Scene {
-        MenuBarExtra("OpenClaw Helper", systemImage: appModel.menuBarSymbol) {
+        MenuBarExtra("ClawRelay", systemImage: appModel.menuBarSymbol) {
             MenuBarPopoverView(viewModel: appModel.menuBarViewModel)
         }
         .menuBarExtraStyle(.window)
 
-        Window("OpenClaw Control Center", id: "control-center") {
+        Window("ClawRelay", id: "control-center") {
             ControlCenterView(viewModel: appModel.controlCenterViewModel)
         }
     }
